439d0ae9654a125a7386686a38102a0fa7eacaaf
Natnael Danssa <natnael.danssa@gmail.com>
2023-08-10 15:10:31 +0200

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import GlobalStyle from "../styles";
 import { events as eventList } from "../api/db.js";
 import { v4 as uuidv4 } from "uuid";
-import Navigation from "@/components/navigation";
+import Navigation from "/components/navigation";
 import { MainContent } from "@/components/maincontent";
 
 export default function App({ Component, pageProps }) {
