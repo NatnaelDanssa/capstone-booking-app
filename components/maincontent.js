56c32218e2a95de9879588b8d942a26bf0874f3d
Natnael Danssa <natnael.danssa@gmail.com>
2023-08-10 15:31:41 +0200

    Fixing Vercel error message

--- components/maincontent.js
+++ components/maincontent.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
-export const MainContent = styled.main`
+const MainContent = styled.main`
   display: flex;
   flex-direction: column;
   justify-content: center;
   gap: 1rem;
   margin-top: 6rem;
 `;
+
+export default MainContent;
