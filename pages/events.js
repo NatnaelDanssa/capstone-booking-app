c6e860fdb9f3b8522b0d60bbb40b79afd2980060
Natnael Danssa <natnael.danssa@gmail.com>
2023-08-14 15:24:58 +0200

    remove events from events.js

--- pages/events.js
+++ pages/events.js
@@ -1,7 +1,6 @@
 import Navigation from "@/components/Navigation";
 import EventCard from "@/components/EventCard";
 import { useRouter } from "next/router";
-import { events } from "@/api/db.js";
 
 export default function EventPage({ id, title, date, type, description }) {
   const router = useRouter();
