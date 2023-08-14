a1e9f97274fa3f4b830b26c633cea46acc6d43bf
Natnael Danssa <natnael.danssa@gmail.com>
2023-08-14 16:57:27 +0200

    updating code review suggestions

--- pages/events.js
+++ pages/events.js
@@ -1,10 +1,6 @@
-import Navigation from "@/components/Navigation";
 import EventCard from "@/components/EventCard";
-import { useRouter } from "next/router";
 
 export default function EventPage({ id, title, date, type, description }) {
-  const router = useRouter();
-  // const eventsId = router.query.eventsId;
   return (
     <article>
       <EventCard />
