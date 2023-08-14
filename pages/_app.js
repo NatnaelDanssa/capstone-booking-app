ca30f90d4a99a1a2150420eaae04a1e2888dd201
Natnael Danssa <natnael.danssa@gmail.com>
2023-08-14 14:50:03 +0200

    code review updates

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }) {
   // State to store the list of events
   const [events, setEvents] = useState(eventList);
 
-  function addEvent(newEvent) {
+  function handleAdd(newEvent) {
     setEvents([...events, { ...newEvent, id: uuidv4() }]);
   }
 
@@ -29,8 +29,8 @@ export default function App({ Component, pageProps }) {
       <Component
         {...pageProps}
         events={events}
-        addEvent={addEvent}
-        updateEvent={updateEvent}
+        handleAdd={handleAdd}
+        handleUpdate={handleUpdate}
       />
     </>
   );
