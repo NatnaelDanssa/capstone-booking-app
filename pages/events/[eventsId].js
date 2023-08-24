3b2a5f58052fdb3d01592f77797e016cb1b48ea6
Natnael Danssa <natnael.danssa@gmail.com>
2023-08-24 17:47:38 +0200

    artist page

--- pages/events/[eventsId].js
+++ pages/events/[eventsId].js
@@ -1,7 +1,6 @@
 import { styled } from "styled-components";
 import { useRouter } from "next/router";
-
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function EventsDetail({
   handleUpdate,
@@ -15,7 +14,6 @@ export default function EventsDetail({
   const eventsId = router.query.eventsId;
 
   const eventToEdit = events.find((event) => event.id == eventsId);
-  console.log("events", events);
 
   useEffect(() => {
     setEventObject(eventToEdit);
@@ -24,6 +22,7 @@ export default function EventsDetail({
   const handleEditClick = () => {
     setEditMode(!editMode);
   };
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setEventObject((prevEvent) => ({
@@ -37,12 +36,6 @@ export default function EventsDetail({
     handleUpdate(eventObject);
   };
 
-  // const handleDeleteEvent = () => {
-  //   const updatedEvents = events.filter((event) => event.id !== eventToEdit.id);
-  //   handleUpdate(updatedEvents);
-  //   setEventObject(null);
-  // };
-
   const handleDeleteClick = () => {
     const confirmDelete = window.confirm(
       "Are you sure you want to delete this event?"
@@ -51,6 +44,10 @@ export default function EventsDetail({
       handleDeleteEvent(eventsId);
       router.push("/");
     }
+  };
+
+  const handleArtistClick = () => {
+    router.push(`/artist/${eventObject.artistId}`);
   };
 
   if (!eventToEdit) {
@@ -106,14 +103,27 @@ export default function EventsDetail({
           eventObject?.description || ""
         )}
       </DescriptionContainer>
+      <ArtistContainer>
+        <Button onClick={handleArtistClick}>
+          {eventObject?.artistName || ""}
+        </Button>
+        <Button onClick={handleArtistClick}>
+          {eventObject?.artistEmail || ""}
+        </Button>
+        <Button
+          onClick={() => router.push(`/edit-artist/${eventObject.artistId}`)}
+        >
+          Create/Edit Artist Page
+        </Button>
+      </ArtistContainer>
       <ButtonContainer>
         {editMode ? (
           <>
-            <button onClick={handleSaveClick}>Save Changes</button>
-            <button onClick={handleDeleteClick}>Delete Event</button>
+            <Button onClick={handleSaveClick}>Save Changes</Button>
+            <Button onClick={handleDeleteClick}>Delete Event</Button>
           </>
         ) : (
-          <button onClick={handleEditClick}>EDIT EVENT</button>
+          <Button onClick={handleEditClick}>EDIT EVENT</Button>
         )}
       </ButtonContainer>
     </article>
@@ -153,6 +163,26 @@ const TypeContainer = styled.p`
   margin-bottom: 10px;
   padding-bottom: 10px;
 `;
+
+const ArtistContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  padding-left: 20px;
+  margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+  font-size: 18px;
+  background-color: #95091b;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  margin-bottom: 10px;
+  margin-right: 10px;
+`;
+
 const DescriptionContainer = styled.p`
   padding-left: 20px;
   display: flex;
@@ -163,13 +193,10 @@ const DescriptionContainer = styled.p`
   margin-top: 0.5;
   margin-bottom: 10px;
 `;
-const ButtonContainer = styled.button`
-  font-size: 18px;
-  padding-left: 20px;
-  background-color: #95091b;
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  cursor: pointer;
-  margin-bottom: 10px;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  margin-top: 30px;
 `;
